f22bb012666f13822cf589231c3891b54115fd24
MarzanaBegum <marzanaBegum473@gmail.com>
2024-01-01 20:46:06 +0600

    error show

--- routes/ProductRoutes.js
+++ routes/ProductRoutes.js
@@ -10,7 +10,7 @@ router.get("/", async (req, res) => {
 
     res.status(200).send(products);
   } catch (error) {
-    res.status(error.status || 500).send(error.message);
+    res.status(error.status || 500).send(error.message,error);
   }
 });
 
